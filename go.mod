9a56fa4360407736970f0b12d199b81081d186ce
Kevin Parrott <parrott.kevin@gmail.com>
2019-07-28 15:36:04 -0400

    Chapter 1 complete

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/parrott-kevin/monkey
+
+go 1.12
---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/parrott-kevin/monkey/lexer"
+	"github.com/parrott-kevin/monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
